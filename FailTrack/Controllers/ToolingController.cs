ce892268613074ff08c219daee3f00317eed667d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-28 09:27:47 -0600

    Controlador tooling con el metodo para registrar

---
+++ FailTrack/Controllers/ToolingController.cs
@@ -0,0 +1,52 @@
+﻿using FailTrack.Models;
+using FailTrack.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FailTrack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToolingController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ToolingController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create([FromBody] ToolingDto request)
+        {
+            if(request == null)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "No se puede enviar datos vacios"
+                });
+            }
+
+            var newItem = new Tooling
+            {
+                ApplicantName = request.ApplicantName,
+                FaultDescription = request.FaultDescription,
+                IdLine = request.IdLine,
+                IdMachine = request.IdMachine,
+                UpdatedAt = null,
+                IdStatus = 1
+            };
+
+            _context.Tooling.Add(newItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                success = true,
+                message = "Registro exitoso"
+            });
+        }
+    }
+}
