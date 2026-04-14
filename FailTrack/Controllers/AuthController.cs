2061b9950fe03f3605fef3112ef0430e4b231541
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-14 09:51:35 -0600

    feat: controlador auth

---
+++ FailTrack/Controllers/AuthController.cs
@@ -0,0 +1,110 @@
+﻿using FailTrack.Dtos;
+using FailTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FailTrack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(AppDbContext context, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<Users>> Register(UserRegisterDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new Users
+            {
+                UserName = request.Username,
+                PasswordHash = Convert.ToBase64String(passwordHash),
+                PasswordSalt = Convert.ToBase64String(passwordSalt),
+                IdLine = request.IdLine,
+                Role = "Line"
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<string>> Login(UserLoginDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == request.UserName);
+
+            if (user == null) return BadRequest("Usuario no encontrado");
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Contraseña incorrecta");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt)
+        {
+            byte[] hashBytes = Convert.FromBase64String(passwordHash);
+            byte[] saltBytes = Convert.FromBase64String(passwordSalt);
+
+            using (var hmac = new HMACSHA512(saltBytes))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(hashBytes);
+            }
+        }
+
+        private string CreateToken(Users user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("lineId", user.IdLine?.ToString() ?? "0")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
