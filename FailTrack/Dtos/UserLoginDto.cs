31784fb8777058756e75bfc4f4dc77196e362ffb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-14 09:51:27 -0600

    feat: dto registro y login

---
+++ FailTrack/Dtos/UserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FailTrack.Dtos
+{
+    public class UserLoginDto
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ FailTrack/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FailTrack.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public int? IdLine { get; set; }
+    }
+}
