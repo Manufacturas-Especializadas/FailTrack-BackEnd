00bd8e44f52e3ede93636ea194781a2c25393fb5
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-14 09:51:15 -0600

    feat: nuevo modelo user

---
+++ FailTrack/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FailTrack.Models;
+
+public partial class Users
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; }
+
+    public string PasswordHash { get; set; }
+
+    public string PasswordSalt { get; set; }
+
+    public int? IdLine { get; set; }
+
+    public string Role { get; set; }
+
+    public virtual Lines IdLineNavigation { get; set; }
+}
